02b7aae2ec3e44e67b85b2cd4d898292b8209974
kharnagy <kharnagy@inedo.com>
2017-04-18 11:54:56 -0400

    Include AssemblyInfo.cs in each project

--- Mercurial/Mercurial.BuildMasterExtension/Properties/AssemblyInfo.cs
+++ Mercurial/Mercurial.BuildMasterExtension/Properties/AssemblyInfo.cs
@@ -1,5 +1,16 @@
-﻿using Inedo.BuildMaster.Extensibility;
+﻿using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.BuildMaster.Extensibility;
 
+[assembly: AssemblyTitle("Mercurial")]
+[assembly: AssemblyDescription("Source control integration for Mercurial.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
-[assembly: RequiredBuildMasterVersion("5.0.0")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2017")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: RequiredBuildMasterVersion("5.6.0")]
--- Mercurial/Mercurial.OtterExtension/Properties/AssemblyInfo.cs
+++ Mercurial/Mercurial.OtterExtension/Properties/AssemblyInfo.cs
@@ -1,3 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("Otter")]
+[assembly: AssemblyTitle("Mercurial")]
+[assembly: AssemblyDescription("Source control integration for Mercurial.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2017")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
