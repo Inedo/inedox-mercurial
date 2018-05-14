a1c93639efef06e681d75989e55a261bfea5bc31
Ben Lubar <ben.lubar@gmail.com>
2018-05-14 11:08:06 -0500

    Update README.md and LICENSE.

--- Mercurial/InedoExtension/Properties/AssemblyInfo.cs
+++ Mercurial/InedoExtension/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("Mercurial")]
 [assembly: AssemblyDescription("Source control integration for Mercurial.")]
-
-[assembly: ComVisible(false)]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
-[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Hedgehog | InedoProduct.Otter)]
